5519511e3b745c1e9ed91fa3864720a69e02688d
Rishab Paruchuri <rishparu@gmail.com>
2025-07-01 20:46:19 -0400

    Removed authentication

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,14 @@
 import { updateSession } from "@/lib/supabase/middleware";
-import { type NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
+  if (
+    request.nextUrl.pathname.startsWith('/api') ||
+    request.nextUrl.pathname.startsWith('/_next') ||
+    request.nextUrl.pathname.startsWith('/favicon.ico')
+  ) {
+    return NextResponse.next()
+  }
   return await updateSession(request);
 }
 
